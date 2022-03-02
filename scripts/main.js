22c4227449fbc0a73dadf2baa431daa102a7a920
Sam Odell <7421902@isd535.org>
2022-03-02 11:31:51 -0600

    java started

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
 const TEXT_INFO_SELECTOR = '[data-image-role="title"]';
 const PIC_LINK = '[data-image-role="select"]';
+const IMFORMATION = '[data-image-role="information"]';
+
+function Details(infoText) {
+    'use strict'
+    let detailText= document.querySelector(TEXT_INFO_SELECTOR);
+    detailTitle.textContent = infoText;
+}
+
+function picText(pic){
+    'use strict';
+    return pic.getAttribute
+}
