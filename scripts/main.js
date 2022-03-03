71248bba534933320cf820dc0d0b9b2874f89a87
Sam Odell <7421902@isd535.org>
2022-03-03 11:30:15 -0600

    Broken. can't get click

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,53 @@
 const TEXT_INFO_SELECTOR = '[data-image-role="title"]';
 const PIC_LINK = '[data-image-role="select"]';
-const IMFORMATION = '[data-image-role="information"]';
+const INFORMATION = '[data-image-role="information"]';
+const HIDE = 'hidden-detail';
 
 function Details(infoText) {
-    'use strict'
-    let detailText= document.querySelector(TEXT_INFO_SELECTOR);
+    'use strict';
+    let detailTitle= document.querySelector(TEXT_INFO_SELECTOR);
     detailTitle.textContent = infoText;
 }
 
 function picText(pic){
     'use strict';
-    return pic.getAttribute
+    return pic.getAttribute('change-text');
 }
+
+function DetailsFromPic(pic) {
+    'use strict';
+    Details(picText(pic));
+}
+
+function clickedPic(pic) {
+    'use strict';
+    pic.addEventListener('click', function(event){
+        event.preventDefault();
+        DetailsFromPic(pic);
+        showDetails
+    });
+}
+
+function getPicArray () {
+    'use strict';
+    let pics = document.querySelectorAll(TEXT_INFO_SELECTOR);
+    let picArray= [].slice.call(pics);
+    return picArray;
+}
+
+
+function mouseClickHandler() {
+    'use strict';
+    document.body.addEventListener('keyup', function(event) {
+        preventDefault();
+    });
+}
+
+function initialzeEvent() {
+    'use strict';
+    let pics = getPicArray();
+    pics.forEach(clickedPic);
+    mouseClickHandler();
+}
+
+initialzeEvent();
